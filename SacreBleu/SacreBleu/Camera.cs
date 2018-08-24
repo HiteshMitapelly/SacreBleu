82f4973ac15355f2e796c682e44c23122bc336a4
HitheshwarMittapelly <hitheshwar.m@gmail.com>
2018-08-24 17:03:26 -0600

    Trying camera stuff. It still doesnt work. Made some minor changes

--- SacreBleu/SacreBleu/Camera.cs
+++ SacreBleu/SacreBleu/Camera.cs
@@ -21,10 +21,15 @@ namespace SacreBleu
 		}
 		public void Follow(GameObject target)  // target is frog
 		{
+			Vector2 minworldPosition = Vector2.Transform(new Vector2(0,SacreBleuGame._instance._screenHeight), Matrix.Invert(SacreBleuGame._instance._camera.Transform));
+			Vector2 maxworldPosition = Vector2.Transform(new Vector2(0, 0), Matrix.Invert(SacreBleuGame._instance._camera.Transform));
+		
+
 			var position = Matrix.CreateTranslation(
 			  -SacreBleuGame._instance._screenWidth / 2,
-			  -target._position.Y - (target._bounds.Height / 2),
-			  0);
+			 -target._position.Y - (target._bounds.Height / 2)
+			 ,
+			  0); //  MathHelper.Clamp(-target._position.Y - (target._bounds.Height / 2), 0f, 2200f) -target._position.Y - (target._bounds.Height / 2)
 
 			var offset = Matrix.CreateTranslation(
 				SacreBleuGame._instance._screenWidth / 2,
@@ -32,6 +37,7 @@ namespace SacreBleu
 				0);
 
 			Transform = position * offset;
+			MathHelper.Clamp(Transform.Up.Y, SacreBleuGame._instance._screenHeight / 2, 2200f);
 		}
 	}
 }
